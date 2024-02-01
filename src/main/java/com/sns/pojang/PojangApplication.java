e95901213e113c16bd8111d97781e63020791687
BellWin98 <jongseunghan@naver.com>
2024-02-01 20:53:45 +0900

    Initial Commit

---
+++ src/main/java/com/sns/pojang/PojangApplication.java
@@ -0,0 +1,13 @@
+package com.sns.pojang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sns/pojang/Pojang/PojangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.pojang.Pojang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
