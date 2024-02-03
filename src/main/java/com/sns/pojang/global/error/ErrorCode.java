372c52fee63cdf653eac038b1fbf736dc83810c6
BellWin98 <jongseunghan@naver.com>
2024-02-04 03:06:31 +0900

    [Fix] 예외 핸들링 메시지 개선(#3)

--- src/main/java/com/sns/pojang/global/error/ErrorCode.java
+++ src/main/java/com/sns/pojang/global/error/ErrorCode.java
@@ -9,7 +9,6 @@ public enum ErrorCode {
 
     // Common
     INVALID_INPUT_VALUE( 400, "입력값이 유효하지 않습니다."),
-
     METHOD_NOT_ALLOWED(405, "클라이언트가 사용한 HTTP 메서드가 리소스에서 허용되지 않습니다."),
     INTERNAL_SERVER_ERROR( 500, "서버에서 요청을 처리하는 동안 오류가 발생했습니다."),
     ACCESS_DENIED(403, "접근 권한이 없습니다."),
