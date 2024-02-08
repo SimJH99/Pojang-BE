60b1632447ff3d6a55770877233890f73f707d28
BellWin98 <jongseunghan@naver.com>
2024-02-08 18:37:24 +0900

    [Fix] OrderMenu PK 추가(#12)

--- src/main/java/com/sns/pojang/domain/orderMenu/entity/OrderMenu.java
+++ src/main/java/com/sns/pojang/domain/orderMenu/entity/OrderMenu.java
@@ -4,8 +4,14 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
