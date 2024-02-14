bcf379318bd8d9da2b727f7b6c4b4ecf9446e80b
BellWin98 <jongseunghan@naver.com>
2024-02-15 00:31:47 +0900

    [Remove] 불필요 파일 삭제

--- src/main/java/com/sns/pojang/domain/menuCategoryName/controller/MenuCategoryController.java
+++
@@ -1,9 +0,0 @@
-package com.sns.pojang.domain.menuCategoryName.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/menu-categories")
-public class MenuCategoryController {
-}
