175ef32eb29967076827e39bc28067bfa610b37b
SimJH99 <s99s4481@gmail.com>
2024-03-05 13:34:42 +0900

    [Feat/Member] Daum API 관련 dto, 엔티티, Address클래스, 메서드 등 수정(#81)

--- src/main/java/com/sns/pojang/domain/member/entity/Address.java
+++ src/main/java/com/sns/pojang/domain/member/entity/Address.java
@@ -13,5 +13,5 @@ public class Address {
     private String sido; // 시/도
     private String sigungu; // 시/군/구
     private String bname; // 동/리
-    private String roadAddress; // 도로명주소
+    private String roadAddress; // 도로명 주소
 }
