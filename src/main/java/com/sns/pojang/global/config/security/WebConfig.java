aba4bdecb2a9d977b2e9229842c2af6f3e0b6c04
BellWin98 <jongseunghan@naver.com>
2024-03-12 10:51:02 +0900

    [Chore] 인코딩 관련 수정

--- src/main/java/com/sns/pojang/global/config/security/WebConfig.java
+++ src/main/java/com/sns/pojang/global/config/security/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("https://www.qoth-0.shop")
+//                .allowedOrigins("http://localhost:8081")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
